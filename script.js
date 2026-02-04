8f460db1e4fea368ed848de03963df461b15e76a
Gabriel135790 <gabrielcruz20415@gmail.com>
2026-02-04 19:24:13 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function itch() {
+    window.open('https://adriel127.itch.io/blockmaker-adriel', '_blank');
+}
+
+function mobileitch() {
+    window.open('https://adriel127.itch.io/blockmaker-mobile-adriel')
+}
